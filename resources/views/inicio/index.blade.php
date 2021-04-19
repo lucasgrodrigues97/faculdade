7d4690cce8772f187c9dbd320586046d2e67e0d6
lucasgrodrigues97 <lucasgrodrigues97@gmail.com>
2021-04-19 09:19:01 -0300

    Faculdade

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -14,14 +14,11 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4 d-flex justify-content-center">
+                        <div class="col-6 d-flex justify-content-center">
                             <h5><a href="/cursos" class="link">Tela de Cursos</a></h5>
                         </div>
-                        <div class="col-4 d-flex justify-content-center">
+                        <div class="col-6 d-flex justify-content-center">
                             <h5><a href="/alunos">Tela de Alunos</a></h5>
-                        </div>
-                        <div class="col-4 d-flex justify-content-center">
-                            <h5><a href="/xml">Cursos em XML</a></h5>
                         </div>
                     </div>
                 </div>
